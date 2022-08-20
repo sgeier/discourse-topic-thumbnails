cfa425e04179f3a1ff2a9e84d89d84e943aa2c35
Sören Geier <sgeier@adobe.com>
2022-08-20 16:07:12 +0200

    throttle

--- javascripts/discourse/initializers/topic-thumbnails-init.js
+++ javascripts/discourse/initializers/topic-thumbnails-init.js
@@ -27,6 +27,16 @@ export default {
 
   initWithApi(api) {
 
+    function throttle(fn, wait) {
+      var time = Date.now();
+      return function () {
+        if ((time + wait - Date.now()) < 0) {
+          fn();
+          time = Date.now();
+        }
+      }
+    }
+
     api.modifyClass("component:topic-list", {
       pluginId: "topic-thumbnails",
       topicThumbnailsService: service("topic-thumbnails"),
@@ -41,13 +51,14 @@ export default {
       isThumbnailList: readOnly("topicThumbnailsService.displayList"),
       isMasonryList: readOnly("topicThumbnailsService.displayMasonry"),
 
+
       didInsertElement() {
         this._super();
         this.updatePosts();
 
         if (window.ResizeObserver) {
           const observer = new ResizeObserver(() =>
-              this.updatePosts()
+              throttle(this.updatePosts, 500)
           );
           observer.observe(this.element);
           this.set("resizeObserver", observer);
